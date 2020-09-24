68a613ea1dc7f5061242c6bc46f9073bf91d8bd5
LuisPeMoraRod <luismorarod98@gmail.com>
2020-09-23 21:50:27 -0600

    Add observer pattern

--- main.cpp
+++ main.cpp
@@ -2,6 +2,110 @@
 #include "Person.h"
 #include "Singleton.h"
 #include "AbstractFactory.h"
+#include <vector>
+
+class Subject;
+
+class Observer
+{
+public:
+    virtual ~Observer() = default;
+    virtual void Update(Subject&) = 0;
+};
+
+class Subject
+{
+public:
+    virtual ~Subject() = default;
+    void Attach(Observer& o) { observers.push_back(&o); }
+    void Detach(Observer& o)
+    {
+        observers.erase(std::remove(observers.begin(), observers.end(), &o));
+    }
+    void Notify()
+    {
+        for (auto* o : observers) {
+            o->Update(*this);
+        }
+    }
+private:
+    std::vector<Observer*> observers;
+};
+
+class ClockTimer : public Subject
+{
+public:
+
+    void SetTime(int hour, int minute, int second)
+    {
+        this->hour = hour;
+        this->minute = minute;
+        this->second = second;
+
+        Notify();
+    }
+
+    int GetHour() const { return hour; }
+    int GetMinute() const { return minute; }
+    int GetSecond() const { return second; }
+
+private:
+    int hour;
+    int minute;
+    int second;
+};
+
+class DigitalClock: public Observer
+{
+public:
+    explicit DigitalClock(ClockTimer& s) : subject(s) { subject.Attach(*this); }
+    ~DigitalClock() { subject.Detach(*this); }
+    void Update(Subject& theChangedSubject) override
+    {
+        if (&theChangedSubject == &subject) {
+            Draw();
+        }
+    }
+
+    void Draw()
+    {
+        int hour = subject.GetHour();
+        int minute = subject.GetMinute();
+        int second = subject.GetSecond();
+
+        std::cout << "Digital time is " << hour << ":"
+                  << minute << ":"
+                  << second << std::endl;
+    }
+
+private:
+    ClockTimer& subject;
+};
+
+class AnalogClock: public Observer
+{
+public:
+    explicit AnalogClock(ClockTimer& s) : subject(s) { subject.Attach(*this); }
+    ~AnalogClock() { subject.Detach(*this); }
+    void Update(Subject& theChangedSubject) override
+    {
+        if (&theChangedSubject == &subject) {
+            Draw();
+        }
+    }
+    void Draw()
+    {
+        int hour = subject.GetHour();
+        int minute = subject.GetMinute();
+        int second = subject.GetSecond();
+
+        std::cout << "Analog time is " << hour << ":"
+                  << minute << ":"
+                  << second << std::endl;
+    }
+private:
+    ClockTimer& subject;
+};
 
 int main() {
     /**
@@ -21,11 +125,12 @@ int main() {
 
     std::cout << s-> getName()<< std::endl;
     std::cout << r->getName() << std::endl;
-    return 0;
+
 
     /**
      * Abstract Factory
      */
+
     UIFactory* ui = new GtkUIFactory();
 
     /* Use the factory to build interface. */
@@ -42,4 +147,18 @@ int main() {
 
     std::cout << main->getToolkit() << ":"
               << main->getType() << std::endl;
+
+    /**
+     * Observer
+     */
+    ClockTimer timer;
+
+    DigitalClock digitalClock(timer);
+    AnalogClock analogClock(timer);
+
+    timer.SetTime(14, 41, 36);
+
+
+
+    return 0;
 }
